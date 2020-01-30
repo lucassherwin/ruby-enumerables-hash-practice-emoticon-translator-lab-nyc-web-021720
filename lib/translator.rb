4e67f5b3d0554ae3a00f97115a26b715df58161e
Lucas Sherwin <lksherwin@gmail.com>
2020-01-30 20:00:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,6 +11,7 @@ def load_library(path)
   }
   emoticons.each do |key, value|
     emoticon_hash['get_emoticon'][value[0]] = emoticons[key][1]
+    emoticon_hash['get_meaning'][value[1]] = key2
 end
 
 def get_japanese_emoticon
