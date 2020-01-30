db03a874a02c3997f5aad901337616fe51f67674
Lucas Sherwin <lksherwin@gmail.com>
2020-01-30 20:00:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,12 @@ require "yaml"
 def load_library(path)
   emoticons = YAML.load_file('emoticons.yml')
 
-
+  emoticon_hash = {
+    'get_meaning' => {},
+    'get_emoticon' => {}
+  }
+  emoticons.each do |key, value|
+    emoticon_hash['get_emoticon'][value[0]] = emoticons[key][1]
 end
 
 def get_japanese_emoticon
